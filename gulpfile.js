4989d30fbcafd4093ab2b94bf8c7b26f90b4b7cc
unicornDi <kochubeyalex71@gmail.com>
2018-10-31 18:39:09 +0100

    сборка проекта

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,40 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var del = require("del");
+var csso = require('gulp-csso');
+var rename = require("gulp-rename");
+
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
-});
+  .pipe(plumber())
+  .pipe(sass())
+  .pipe(postcss([
+  autoprefixer()
+  ]))
+  .pipe(gulp.dest("build/css"))
+  .pipe(csso())
+  .pipe(rename("style.min.css"))
+  .pipe(gulp.dest("build/css"));
+ });
+
+ gulp.task("sprite", function () {
+  return gulp.src("source/img/icon-*.svg")
+  .pipe(svgstore({
+  inlineSvg: true
+  }))
+  .pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("build/img"));
+ });
+
+ gulp.task("html", function () {
+  return gulp.src("source/*.html")
+  .pipe(posthtml([
+  include()
+  ]))
+  .pipe(gulp.dest("build"));
+ });
 
 gulp.task("server", function () {
   server.init({
@@ -32,3 +55,27 @@ gulp.task("server", function () {
 });
 
 gulp.task("start", gulp.series("css", "server"));
+
+gulp.task("copy", function () {
+ return gulp.src([
+ "source/fonts/**/*.{woff,woff2}",
+ "source/img/**",
+ "source/js/**"
+ ], {
+ base: "source"
+ })
+ .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+ });
+
+ gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "sprite",
+  "html"
+ ));
+
